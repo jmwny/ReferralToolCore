649f82a2903054036447c5c0b38acb8ff7816f33
jmwny <jmwny@users.noreply.github.com>
2024-08-05 11:46:03 -0400

    Stripped

--- ReferralToolCore/Views/MainWindow.xaml.cs
+++ ReferralToolCore/Views/MainWindow.xaml.cs
@@ -1,10 +1,6 @@
-﻿using ReferralToolCore.Models;
-using ReferralToolCore.ViewModels;
-using System;
-using System.Net.NetworkInformation;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace ReferralToolCore.Views
 {
@@ -13,12 +9,12 @@ namespace ReferralToolCore.Views
     /// </summary>
     public partial class MainWindow : Window
     {
-        string referralReceived = "Referral received. Please standby while we review this referral.";
-        string authRequiredMAS = "Trip #TRIPNUM was released to (insert vendor name).  Please stand by while we contact insurance to set up transportation. Thank you.";
-        string closedReferralLivery = "Trip #TRIPNUM on (insert date only) arranged with Medicaid insurance. They state they may take up to 3hrs to assign a vendor. All Livery request vendors will call the patient directly at the number you provided. **Referral is now closed and will no longer be viewed. PLEASE CALL IN ANY CHANGES. DO NOT EDIT. ** Thank you.";
-        string closedReferralNextAvail = "Trip #TRIPNUM confirmed on (insert date) with (insert vendor name). The vendor is granted a 3hr window to arrive on scene. You can expect their arrival by (insert standard time) for this request. **Referral is now closed and will no longer be viewed. PLEASE CALL IN ANY CHANGES. DO NOT EDIT. ** Thank you.";
-        string closedReferralPreSched = "Trip #TRIPNUM on DATETIME with (insert vendor name). **Referral is now closed and will no longer be viewed. PLEASE CALL IN ANY CHANGES. DO NOT EDIT. ** Thank you.";
-        string insuranceAuth = "Trip #TRIPNUM Insurance verification of non-emergent transportation benefits along with authorization from the primary insurance listed below needs to be obtained by your department for the vendor. If no authorization is required, kindly provide the name and contact number of the representative so we can provide it to the vendor. Thank you.";
+        readonly string referralReceived = "Referral received. Please standby while we review this referral.";
+        readonly string authRequiredMAS = "Trip #TRIPNUM was released to (insert vendor name).  Please stand by while we contact the insurance to set up transportation. Thank you.";
+        readonly string closedReferralLivery = "Trip #TRIPNUM on (insert date only) arranged with Medicaid insurance. They state they may take up to 3hrs to assign a vendor. All Livery request vendors will call the patient directly at the number you provided. **Referral is now closed and will no longer be viewed. PLEASE CALL IN ANY CHANGES. DO NOT EDIT. ** Thank you.";
+        readonly string closedReferralNextAvail = "Trip #TRIPNUM confirmed on (insert date) with (insert vendor name). The vendor is granted a 3hr window to arrive on scene. You can expect their arrival by (insert standard time) for this request. **Referral is now closed and will no longer be viewed. PLEASE CALL IN ANY CHANGES. DO NOT EDIT. ** Thank you.";
+        readonly string closedReferralPreSched = "Trip #TRIPNUM on DATETIME with (insert vendor name). **Referral is now closed and will no longer be viewed. PLEASE CALL IN ANY CHANGES. DO NOT EDIT. ** Thank you.";
+        readonly string insuranceAuth = "Trip #TRIPNUM Insurance verification of non-emergent transportation benefits along with authorization from the primary insurance listed below needs to be obtained by your department for the vendor. If no authorization is required, kindly provide the name and contact number of the representative so we can provide it to the vendor. Thank you.";
 
         public MainWindow()
         {
@@ -30,36 +26,6 @@ namespace ReferralToolCore.Views
             Btn_closedReferralNextAvail.Content = closedReferralNextAvail;
             Btn_closedReferralPreSched.Content = closedReferralPreSched;
             Btn_insuranceAuth.Content = insuranceAuth;
-        }
-
-        // Enter new referral dialog
-        private void TabControl_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            (DataContext as ViewModel).NewReferralButtonClick();
-        }
-
-        // Active tabs (MAS and LGTC) view referral
-        private void ListViewActive_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            (DataContext as ViewModel).ViewItemClick();
-        }
-
-        // History tab button
-        private void HistoryRefreshButton_Click(object sender, RoutedEventArgs e)
-        {
-            (DataContext as ViewModel).HistoryRefreshButtonClick();
-        }
-
-        // History tab - view referral dialog
-        private void ListViewHistory_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            (DataContext as ViewModel).HistoryListViewClick();
-        }
-
-        private void ComboBox_SourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
-        {
-            ReferralData rdata = (ReferralData)(sender as ComboBox).DataContext;
-            (DataContext as ViewModel).UpdateReferralCallStatus(rdata);
         }
 
         private void Btn_Checked(object sender, RoutedEventArgs e)
